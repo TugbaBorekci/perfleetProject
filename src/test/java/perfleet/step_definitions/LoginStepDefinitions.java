1a2ae3401166796efad1da5e36190a447c806156
Tugba Borekci <tugbaborekci80@gmail.com>
2023-03-30 17:35:25 +0100

    PerfleetProjectCreated

--- src/test/java/perfleet/step_definitions/LoginStepDefinitions.java
+++ src/test/java/perfleet/step_definitions/LoginStepDefinitions.java
@@ -15,20 +15,19 @@ public class LoginStepDefinitions {
     LoginPage loginPage = new LoginPage();
     HomePage homePage = new HomePage();
 
-
     @Given("User is on the  login page")
     public void user_is_on_the_login_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-
     }
+
 
     @When("The user logs in as a driver")
     public void the_user_logs_in_as_a_driver() {
         loginPage.inputUsername.sendKeys(ConfigurationReader.getProperty("driver_username"));
         loginPage.inputPassword.sendKeys(ConfigurationReader.getProperty("all_password"));
         loginPage.loginButton.click();
-
     }
+
 
     @Then("The user is on the Quick Launchpad page")
     public void the_user_is_on_the_quick_launchpad_page() throws InterruptedException {
@@ -37,8 +36,8 @@ public class LoginStepDefinitions {
         String actualTitle = homePage.quickLaunchpad.getText();
 
         Assert.assertEquals(expectedTitle, actualTitle);
-
     }
+
 
     @When("The user logs in as a sales manager")
     public void the_user_logs_in_as_a_sales_manager() {
@@ -47,12 +46,14 @@ public class LoginStepDefinitions {
         loginPage.loginButton.click();
     }
 
+
     @When("The user logs in as a store manager")
     public void the_user_logs_in_as_a_store_manager() {
         loginPage.inputUsername.sendKeys(ConfigurationReader.getProperty("store_manager_username"));
         loginPage.inputPassword.sendKeys(ConfigurationReader.getProperty("all_password"));
         loginPage.loginButton.click();
     }
+
 
     @Then("The user is on the Dashboard page")
     public void the_user_is_on_the_dashboard_page() throws InterruptedException {
@@ -61,21 +62,22 @@ public class LoginStepDefinitions {
         String actualTitle = homePage.dashboard.getText();
 
         Assert.assertEquals(expectedTitle, actualTitle);
-
     }
+
 
     @When("The user enters invalid username and password")
     public void the_user_enters_invalid_username_and_password() {
         loginPage.inputUsername.sendKeys(ConfigurationReader.getProperty("invalid_username"));
         loginPage.inputPassword.sendKeys(ConfigurationReader.getProperty("invalid_password"));
         loginPage.loginButton.click();
-
     }
+
 
     @Then("The user sees 'Invalid username or password'")
     public void the_user_sees_invalid_username_or_password() {
         homePage.invalidUsernameOrPassword.isDisplayed();
     }
+
 
     @When("The user leaves the username or password input box empty")
     public void the_user_leaves_the_username_or_password_input_box_empty() {
@@ -83,30 +85,30 @@ public class LoginStepDefinitions {
         loginPage.loginButton.click();
     }
 
+
     @Then("The user sees 'Please fill in this field' warning message")
     public void the_user_sees_warning_message_please_fill_in_this_field() {
 
         String expectedMessage = "Please fill in this field.";
         String actualMessage = loginPage.inputUsername.getAttribute("validationMessage");
         Assert.assertEquals(expectedMessage, actualMessage);
-
-
     }
+
 
     @When("The user leaves both username and password input boxes are empty and clicks on login button")
     public void the_user_leaves_both_username_and_password_input_boxes_are_empty_and_clicks_on_login_button() {
         loginPage.loginButton.click();
-
     }
+
 
     @Then("The user sees 'Please fill in this field' warning message only in the username input box")
     public void the_user_sees_warning_message_only_in_the_username_input_box_please_fill_in_this_field() {
-
 
         String expectedMessage = "Please fill in this field.";
         String actualMessage = loginPage.inputUsername.getAttribute("validationMessage");
         Assert.assertEquals(expectedMessage, actualMessage);
     }
+
 
     @Then("The driver sees their own username in the profile menu")
     public void the_driver_sees_their_own_username_in_the_profile_menu() throws InterruptedException {
@@ -114,7 +116,6 @@ public class LoginStepDefinitions {
         String expectedProfileName = ConfigurationReader.getProperty("driver_username");
         String actualProfileName = homePage.userProfile.getText();
         Assert.assertEquals(expectedProfileName, actualProfileName);
-
     }
 
 
@@ -124,7 +125,6 @@ public class LoginStepDefinitions {
         String expectedProfileName = ConfigurationReader.getProperty("sales_manager_username");
         String actualProfileName = homePage.userProfile.getText();
         Assert.assertEquals(expectedProfileName, actualProfileName);
-
     }
 
 
